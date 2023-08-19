139ce7693d47cc1c25e8f9100d2352e5dfac7798
sravaani <sravanimandadapu19@gmail.com>
2023-08-19 15:48:53 +0530

    Added createAsyncThunk

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,23 @@ import axios from 'axios';
 import './App.css'
 import Navbar from './components/Navbar';
 import {useDispatch, useSelector} from 'react-redux';
-import { addData } from "./features/getSlice";
+import  addData  from "./reducers/phoneReducer";
 import Data from "./pages/Data";
 import { useEffect } from "react";
+import { getMobileList } from './services/mobileAction';
 
 function App() {
  const dispatch = useDispatch();
- const apiData = useSelector((state) => state.getData);
-  async function getApi(){
-    const res = await  axios.get('https://dummyjson.com/products');
-    const data =  await res.data.products;
-    dispatch(addData(data))
-  }
- 
+ const apiData = useSelector((state) => state.phoneReducer.phoneList);
+  // async function getApi(){
+  //   const res = await  axios.get('https://dummyjson.com/products');
+  //   const data =  await res.data.products;
+  //   dispatch(addData(data))
+  // }
+ console.log(apiData, 'Api called');
 useEffect(() => {
-getApi();
+  console.log('use Effect called');
+   dispatch( getMobileList())
 },[])
   return (
     <div className="App">
