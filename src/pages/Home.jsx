e219fe73ed875a067112d5c4b528898b37ead3c3
sravaani <sravanimandadapu19@gmail.com>
2023-08-22 22:43:25 +0530

    Added State

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Form from '../components/Form';
 import { Suspense } from 'react';
+import State from './State';
 
 const Home = () => {
   return (
     <div className="text-center">
       <Navbar/>
       <h1 className= "mt-10 text-2xl">Home</h1>
+      <State/>
       <Form/>
     </div>
   )
